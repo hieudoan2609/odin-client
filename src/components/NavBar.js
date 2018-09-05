d927ba0fc91274dd1afe8f93c1e65372d7156683
Hieu <hieudoan2609@gmail.com>
2018-09-05 17:00:50 +0700

    update logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class NavBar extends Component {
-  render() {
-    return (
-      <div className="nav-bar">
-        <div className="container">
-          <div className="brand-logo">
-            <Link to="/">
-              <img src="/blacklotus-with-shadow.png" alt="Black Lotus" />
-            </Link>
-          </div>
-          <div className="nav-items">
-            <ul className="pull-right" />
-            <ul className="pull-left">
-              <Link to="/account">
-                <li className="nav-item">
-                  <i className="icon ion-ios-wallet" /> 0x8a37b7...2830E1{' '}
-                  (0.097252 BTC)
-                </li>
-              </Link>
-            </ul>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="nav-bar">
+				<div className="container">
+					<div className="brand-logo">
+						<Link to="/">
+							<img src="/logo.png" alt="OdinTrade" />
+						</Link>
+					</div>
+					<div className="nav-items">
+						<ul className="pull-right" />
+						<ul className="pull-left">
+							<Link to="/account">
+								<li className="nav-item">
+									<i className="icon ion-ios-wallet" /> 0x8a37b7...2830E1{" "}
+									(0.097252 BTC)
+								</li>
+							</Link>
+						</ul>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default NavBar;
